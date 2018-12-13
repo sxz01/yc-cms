799f2a220303561f0271ef0172b4631f57324e07
sxz01 <sxz_vip@126.com>
2018-12-13 11:31:18 +0800

    初始化

---
+++ yc-cms-common/src/main/java/com/yuancore/cms/App.java
@@ -0,0 +1,13 @@
+package com.yuancore.cms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
